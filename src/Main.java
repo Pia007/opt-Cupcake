0ba0375055fb7063a7742a2227c4e0d5b217a065
Pia <piatorain@yahoo.com>
2022-09-26 10:58:36 -0700

    wrapped scanner in try block

--- src/Main.java
+++ src/Main.java
@@ -22,45 +22,46 @@ public class Main {
                 "\nthe menu but we need to decide on pricing");
 
         // create a scanner object
-        Scanner input = new Scanner(System.in);
+        // wrapped in a try block to avoid having to close the scanner
+        try (
+                Scanner input = new Scanner(System.in)) {
+            System.out.println("We are deciding on the price for our standard cupcake. Here is the description: ");
 
-        System.out.println("We are deciding on the price for our standard cupcake. Here is the description: ");
+            // type() method - print the description of the generic cupcake
+            cupcake.type();
 
-        // type() method - print the description of the generic cupcake
-        cupcake.type();
+            // print how much would you like to charge for the generic cupcake
+            System.out.println("How much would you like to charge for the cupcake?" +
+                    "\n(Input a numerical number taken to 2 decimals)");
 
-        // print how much would you like to charge for the generic cupcake
-        System.out.println("How much would you like to charge for the cupcake?" +
-                "\n(Input a numerical number taken to 2 decimals)");
+            // string variable to stor the user input
+            String priceText = input.nextLine();
 
-        // string variable to stor the user input
-        String priceText = input.nextLine();
+            // convert the string to a double
+            double price = Double.parseDouble(priceText);
 
-        // convert the string to a double
-        double price = Double.parseDouble(priceText);
+            // call the setPrice() method, with price as the parameter
+            cupcake.setPrice(price);
 
-        // call the setPrice() method, with price as the parameter
-        cupcake.setPrice(price);
-
-        //red velvet cupcake
-        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description: ");
-        redVelvet.type();
-        System.out.println("How much would you like to charge for the cupcake?" +
-                "\n(Input a numerical number taken to 2 decimals)");
-        priceText = input.nextLine();
-        price = Double.parseDouble(priceText);
-        redVelvet.setPrice(price);
+            //red velvet cupcake
+            System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description: ");
+            redVelvet.type();
+            System.out.println("How much would you like to charge for the cupcake?" +
+                    "\n(Input a numerical number taken to 2 decimals)");
+            priceText = input.nextLine();
+            price = Double.parseDouble(priceText);
+            redVelvet.setPrice(price);
 
 
-        // chocolate cupcake
-        System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description: ");
-        chocolate.type();
-        System.out.println("How much would you like to charge for the cupcake?" +
-                "\n(Input a numerical number taken to 2 decimals)");
-        priceText = input.nextLine();
-        price = Double.parseDouble(priceText);
-        chocolate.setPrice(price);
-
+            // chocolate cupcake
+            System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description: ");
+            chocolate.type();
+            System.out.println("How much would you like to charge for the cupcake?" +
+                    "\n(Input a numerical number taken to 2 decimals)");
+            priceText = input.nextLine();
+            price = Double.parseDouble(priceText);
+            chocolate.setPrice(price);
+        }
         // add the cupcake objects to the cupcakeMenu ArrayList
         cupcakeMenu.add(cupcake);
         cupcakeMenu.add(redVelvet);
